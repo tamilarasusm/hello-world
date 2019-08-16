fdc76f6e97b152c892a9029be0a154a261352ef1
Tamilarasu SM <tamilarasusm@gmail.com>
2019-08-16 14:58:15 +0530

    Added Hello World Program

---
+++ src/in/tamilarasu/jenkinsdemo/hello/Hello.java
@@ -0,0 +1,13 @@
+package in.tamilarasu.jenkinsdemo.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<10;i++) {
+			System.out.println("Hello World... " + (i+1));
+		}
+
+	}
+
+}
